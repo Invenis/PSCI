ae5dd4e8098310591009d55ffdb80146f1f5cbbd
mgrzywa <mgrzywa@objectivity.co.uk>
2017-05-21 10:14:09 +0200

    Build fix.

--- _buildTools/build.ps1
+++ _buildTools/build.ps1
@@ -23,9 +23,10 @@ else {
 }
 
 "Installing build dependencies"
-Invoke-PSDepend -Path "$PSScriptRoot\build.depend.psd1" -Force -Verbose -Import
+Invoke-PSDepend -Path "$PSScriptRoot\build.depend.psd1" -Force -Verbose
 "Installing project dependencies"
-Invoke-PSDepend -Path "$PSScriptRoot\psci.depend.psd1" -Target "$PSScriptRoot\..\baseModules" -Force -Verbose -Import
+Remove-Item -Path "$PSScriptRoot\..\baseModules" -Recurse -Force -ErrorAction SilentlyContinue
+Invoke-PSDepend -Path "$PSScriptRoot\psci.depend.psd1" -Target "$PSScriptRoot\..\baseModules" -Force -Verbose
 
 ### Run psake
 "Setting build environment"
